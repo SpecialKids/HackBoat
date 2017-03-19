b6524d493ee08908205c688c2f5c862fc94a836b
Volcyy <Johannes.christ@gmail.com>
2017-03-19 15:30:19 +0100

    Move Authorized Eval IDs to Config

--- config/config.exs
+++ config/config.exs
@@ -2,10 +2,12 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-
 config :hackBoat,
-  # Add either your Bot Token or your Local Storage Token here.
-  token: "ADD HERE",
+  # Put either your Bot Token or your Local Storage Token (for Selfbots) here.
+  token: "",
 
-  # Add your self-ID if you want to use the Bot as a Selfbot. Leave empty if this is not the case.
+  # Put User IDs that can use Evaluation Commands without restriction here.
+  authorized: ["", ""],
+
+  # Add your User ID here if you want to use the Bot as a Selfbot. Leave empty if this is not the case.
   self_id: ""
