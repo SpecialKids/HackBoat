8fdd863d7607481959735c9aa186c63c33979ec4
Volcyy <Johannes.christ@gmail.com>
2017-03-19 15:18:50 +0100

    Add support for running as Selfbot

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,10 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+
 config :hackBoat,
-  token: "ADD HERE"
+  # Add either your Bot Token or your Local Storage Token here.
+  token: "ADD HERE",
+
+  # Add your self-ID if you want to use the Bot as a Selfbot. Leave empty if this is not the case.
+  self_id: ""
--- lib/hackboat.ex
+++ lib/hackboat.ex
@@ -6,10 +6,16 @@ defmodule HackBoat do
   alias Alchemy.Client
 
   @token Application.fetch_env!(:hackBoat, :token)
-
+  @self_id Application.fetch_env!(:hackBoat, :self_id)
 
   def start(_, _) do
-    run = Client.start(@token)
+    run =
+      if @self_id == "" do
+        Client.start(@token)
+      else
+        Client.start(@token, selfbot: @self_id)
+      end
+
     use HackBoat.Elixir.Eval
     use HackBoat.Learn
     run
