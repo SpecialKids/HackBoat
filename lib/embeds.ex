5126bb27a907395c746b01524924c99bb92c2709
Volcyy <Johannes.christ@gmail.com>
2017-03-19 18:09:26 +0100

    Add embeds.ex to ease usage of Embeds. Uses overloading for simplicity.

---
+++ lib/embeds.ex
@@ -0,0 +1,115 @@
+defmodule HackBoat.Embeds do
+  @moduledoc false
+  # Provides Functions to ease the creation of Embeds.
+
+  import Alchemy.Embed
+
+  @doc false
+  #### color_picker/1
+  ## Get an integer representing a colour based on the input name.
+  #
+  ## Arguments
+  #    - color_name: String denoting the Color Name.
+  #
+  ## Example
+  #    pick_colour("red")
+  defp pick_colour(color_name) do
+    case color_name do
+      "red" -> 0xCC0000
+      "blue" -> 0x446BEB
+      "green" -> 0x39DB4C
+      "pink" -> 0xDA2FE0
+      "brown" -> 0xE0822F
+    end
+  end
+
+  @doc false
+  ### code_block/1
+  ## Create a Code Block.
+  #
+  ## Arguments
+  #    - code: String containing the Code which should be put in the Code block.
+  #
+  ## Example
+  #    code_block("2 + 2")
+  defp code_block(code) do
+    "```\n#{code}\n```"
+  end
+
+  @doc false
+  #### code_block/2
+  ## Create a Code Block with Syntax Highlighting.
+  #
+  ## Arguments
+  #    - lang: String denoting the Language for which the Code Block should be created.
+  #    - code: String containing the Code that should be put in the Code block.
+  #
+  ## Example
+  #    code_block("python", "print('hello world')")
+  defp code_block(lang, code) do
+    "```#{lang}\n#{code}\n```"
+  end
+
+  @doc false
+  #### title_only/1
+  ## Create an Embed containing just a Title.
+  #
+  ## Arguments
+  #    - title: String specifying the Title
+  #
+  ## Example
+  #    title_only("Hello!")
+  def title_only(title) do
+    %Alchemy.Embed{}
+    |> title(title)
+  end
+
+  @doc false
+  #### title_only/2
+  ## Create a colored Embed with just a Title.
+  #
+  ## Arguments
+  #    - title: String specifying the Title
+  #    - color_name: String denoting the Color to be used.
+  #
+  ## Example
+  #     title_only("Error", "red")
+  def title_only(title, color_name) do
+    %Alchemy.Embed{}
+    |> title(title)
+    |> color(pick_colour(color_name))
+  end
+
+  @doc false
+  #### eval_result/2
+  ## Create an Embed to display Code Evaluation Results.
+  #
+  ## Arguments
+  #    - input: String containing the inputted Code.
+  #    - output: String containing the Evaluation result.
+  #
+  ## Example
+  #    eval_result("print('hello world')", "'hello world'")
+  def eval_result(input, output) do
+    %Alchemy.Embed{}
+    |> field("Input:", code_block())
+  end
+
+  @doc false
+  #### eval_result/3
+  ## Create an Embed with Syntax Highlighting to display Code Evaluation Results.
+  #
+  ## Arguments
+  #    - input: String containing the inputted Code.
+  #    - output: String containing the Evaluation result.
+  #    - lang: String denoting the used language.
+  #
+  ## Example
+  #    eval_result("print('hello world')", "'hello world'", "python")
+  def eval_result(input, output, lang) do
+    %Alchemy.Embed{}
+    |> field("Input:", code_block(lang, input))
+    |> field("Output:", code_block(lang, output))
+  end
+
+end
